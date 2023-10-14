e9f41b81f049fbae0c6a4b671749422800e854c7
jerc777 <javieredo.rivera@gmail.com>
2023-10-14 23:43:35 +0200

    Update Ejercicio 3.sql

--- Ejercicio 3.sql
+++ Ejercicio 3.sql
@@ -1,7 +1,7 @@
 --Ejercicios 3
 --1. Crea una tabla llamada "Productos" con las columnas: "id" (entero, clave primaria), "nombre" (texto) y "precio" (numérico).
 CREATE TABLE IF NOT EXISTS productos(
-	id integer PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
 	nombre VARCHAR(255) NOT NULL,
 	precio numeric NOT NULL	
 )
@@ -17,7 +17,7 @@ VALUES (1,'patatas', 1.5),
 
 --3. Actualiza el precio de un producto en la tabla "Productos".
 UPDATE productos
-SET nombre = 'patatas', precio=0.9
+SET nombre = 'Patatas', precio=0.9
 WHERE id = 1;
 
 --4. Elimina un producto de la tabla "Productos".
@@ -26,31 +26,24 @@ where id=5
 
 --5. Realiza una consulta que muestre los nombres de los usuarios junto con los nombres de los productos que han comprado (utiliza un INNER JOIN con la tabla "Productos").
 
--- Con esta sentencia agrego la columna Usuarios_id para usarla de FK
-ALTER TABLE public.productos
-ADD usuarios_id int;
-
--- con esta sentencia actualizo el indice de usuario_id de los productos que ya están
-
-UPDATE productos
-SET usuarios_id = 1
-WHERE id = 1;
-
--- y con esta creo  nuevas
-
-INSERT INTO public.productos(id,nombre, precio, usuarios_id)
-VALUES (7,'patatas', 0.9, 2);
-(8,'Huevos', 2.8, 3),
-(9,'Aceite', 8, 3),
-(10,'Cebolla', 1.6, 3),
-(11,'Ajo', 0.8, 4),
-(12,'Cebolla', 1.6, 4);
+--Primero crearemos la tabla usuarios y la llenamos
+CREATE TABLE IF NOT EXISTS Clientes (
+	ID SERIAL	PRIMARY KEY,
+	NAME VARCHAR(255) NOT NULL,
+	EMAIL VARCHAR(255)
+)
+INSERT INTO public.clientes (name, email)
+VALUES ('Juan', 'juan@example.com'),
+('Diego', 'diego@gmail.com'),
+('Pedro','pedro@gmail.com'),
+('suana','susana@gmail.com'),
+('Ana', 'ana@gmail.com');
 
 --Ahora hago la consulta
 
-SELECT * 
-FROM public.usuarios A
+SELECT A.id, A.name, B.nombre
+FROM public.clientes A
 INNER JOIN public.productos B
-on A.id = B.usuarios_id
+on A.id = B.id
 ORDER BY A.id ASC
 
